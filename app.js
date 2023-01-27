4c3bbe1da65a3104bdcfcb7df374f04374e59e35
Dzhaneta Kabertay <dj-jen8@yandex.ru>
2023-01-28 01:18:16 +0300

    commit for review

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
 app.use((req, res) => {
-  res.status(404).send({ message: 'Карточка с указанным _id не найдена.' });
+  res.status(404).send({ message: 'Указанная страница не найдена.' });
 });
 
 app.listen(PORT);
